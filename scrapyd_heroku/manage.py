b3fb8fa073450926054dcc298c8327b507c05e75
rubnet <1204075579@qq.com>
2018-11-30 23:11:11 +0800

    Update manage.py
    decode

--- scrapyd_heroku/manage.py
+++ scrapyd_heroku/manage.py
@@ -6,6 +6,15 @@ from sys import argv
 from twisted.scripts.twistd import run
 
 import scrapyd_heroku as project
+import os
+import base64
+
+
+env = os.environ
+domain = base64.b64decode(env['DOMAIN'].encode('ascii'))
+ip = base64.b64decode(env['IP'].encode('ascii'))
+user = base64.b64decode(env['USER'].encode('ascii'))
+passwd = base64.b64decode(env['PASSWD'].encode('ascii'))
 
 
 def main():
